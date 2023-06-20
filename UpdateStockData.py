86e34e74ed821c249e1c61b05b274843f78efe77
Pablo Fauda <42186966+pfauda@users.noreply.github.com>
2023-06-20 13:49:59 -0300

    Refactoring & progress bar

--- UpdateStockData.py
+++ UpdateStockData.py
@@ -1,11 +1,11 @@
 import sqlite3
 from sqlite3 import Error
-#from sqlite3.dbapi2 import Cursor
 from sqlite3.dbapi2 import Connection
 import yfinance as yf
 import datetime as dt
 import numpy as np
 import getpass
+from tqdm import tqdm
 
 from pyhomebroker import HomeBroker
 
@@ -19,19 +19,21 @@ def create_connection(db_file) -> Connection:
     try:
         sqlite3.register_adapter(np.int64, int)
         sqlite3.register_adapter(np.int32, int)
-        conn = sqlite3.connect(db_file)
+        conn = sqlite3.connect(db_file, cached_statements=0)
     except Error as e:
-        print(e)
+        print(str(e))
 
     return conn  # type: ignore
 
-def insert_stock_prices_from(conn, stocks_prices):
+def insert_stock_prices_from(conn, stocks_prices: np.recarray):
 
-    sql = ''' INSERT INTO stocks(Name, Date, Open, High, Low, Close, AdjClose, Volume)
-              VALUES(?, ?, ?, ?, ?, ?, ?, ?) '''
-    cur = conn.cursor()
-    cur.executemany(sql, stocks_prices)
-    conn.commit()
+    if stocks_prices.size > 0:
+
+        sql = ''' INSERT INTO stocks(Name, Date, Open, High, Low, Close, AdjClose, Volume)
+                  VALUES(?, ?, ?, ?, ?, ?, ?, ?) '''
+        cur = conn.cursor()
+        cur.executemany(sql, stocks_prices)
+        conn.commit()
 
 def select_last_stock_price(conn, stock_name):
 
@@ -48,56 +50,85 @@ def select_stocks_symbols(conn):
     cur.execute(sql)
     return cur.fetchall()
 
-def get_stock_prices_from_yf(conn, symbol):
+def get_stock_prices_from_yf(conn, symbol) -> np.recarray:
 
     str_start_date = select_last_stock_price(conn, symbol)
-    start_date = dt.datetime(int(str_start_date[0:4]), int(str_start_date[5:7]), int(str_start_date[8:10]))
-    start_date += dt.timedelta(days=1)
 
-    df_stocks_prices = yf.download(symbol,
-        start_date,
-        group_by='row',
-        interval='1d',
-        progress=False,
-        threads=False,
-        auto_adjust=False,
-        rounding=True)
+    result = np.recarray((0, ), dtype=[("Name", str), 
+                                       ("Date", str),
+                                       ("Open", float),
+                                       ("High", float),
+                                       ("Low", float),
+                                       ("Close", float),
+                                       ("AdjClose", float),
+                                       ("Volume", int)])
 
-    df_stocks_prices.fillna(method='ffill', axis=0, inplace=True)
+    if dt.datetime.strptime(str_start_date, "%Y-%m-%d").date() < dt.datetime.today().date():
 
-    df_stocks_prices.reset_index(inplace=True)
-    df_stocks_prices.insert(0, 'Name', symbol)
-    df_stocks_prices.drop(df_stocks_prices[df_stocks_prices.Date <= str_start_date].index, inplace=True)
-    df_stocks_prices['Date'] = df_stocks_prices['Date'].apply(lambda x: dt.datetime.strftime(x, '%Y-%m-%d'))
-    df_stocks_prices.rename(columns={'Adj Close': 'AdjClose'}, inplace=True)
-    return df_stocks_prices.to_records(index=False)
+        start_date = dt.datetime(int(str_start_date[0:4]), int(str_start_date[5:7]), int(str_start_date[8:10]))
+        start_date += dt.timedelta(days=1)
 
-def get_stock_prices_from_hb(conn, hb, symbol):
+        df_stocks_prices = yf.download(symbol,
+            start_date,
+            group_by='row',
+            interval='1d',
+            progress=False,
+            threads=False,
+            auto_adjust=False,
+            rounding=True)
+
+        df_stocks_prices.fillna(method='ffill', axis=0, inplace=True)
+
+        df_stocks_prices.reset_index(inplace=True)
+        df_stocks_prices.insert(0, 'Name', symbol)
+        df_stocks_prices.drop(df_stocks_prices[df_stocks_prices.Date <= str_start_date].index, inplace=True)
+        df_stocks_prices['Date'] = df_stocks_prices['Date'].apply(lambda x: dt.datetime.strftime(x, '%Y-%m-%d'))
+        df_stocks_prices.rename(columns={'Adj Close': 'AdjClose'}, inplace=True)
+        result = df_stocks_prices.to_records(index=False)
+    
+    return result
+
+def get_stock_prices_from_hb(conn, hb, symbol) -> np.recarray:
+
 
     str_start_date = select_last_stock_price(conn, symbol + '.BA')
-    start_date = dt.datetime(int(str_start_date[0:4]), int(str_start_date[5:7]), int(str_start_date[8:10]))
-    start_date += dt.timedelta(days=1)
+ 
+    result = np.recarray((0, ), dtype=[("Name", str), 
+                                       ("Date", str),
+                                       ("Open", float),
+                                       ("High", float),
+                                       ("Low", float),
+                                       ("Close", float),
+                                       ("AdjClose", float),
+                                       ("Volume", int)])
+    
+    if dt.datetime.strptime(str_start_date, "%Y-%m-%d").date() < dt.datetime.today().date():
 
-    df_stocks_prices = hb.history.get_daily_history(symbol, start_date.date(), dt.datetime.today().date())
+       start_date = dt.datetime(int(str_start_date[0:4]), int(str_start_date[5:7]), int(str_start_date[8:10]))
+       start_date += dt.timedelta(days=1)
 
-    df_stocks_prices.insert(0, 'Name', symbol + '.BA')
-    col_map = {df_stocks_prices.columns[1]: 'Date', 
-               df_stocks_prices.columns[2]: 'Open', 
-               df_stocks_prices.columns[3]: 'High', 
-               df_stocks_prices.columns[4]: 'Low', 
-               df_stocks_prices.columns[5]: 'Close', 
-               df_stocks_prices.columns[6]: 'Volume'}
-    df_stocks_prices.rename(columns=col_map, inplace=True)
-    df_stocks_prices.insert(6, 'AdjClose', df_stocks_prices['Close'])
+       df_stocks_prices = hb.history.get_daily_history(symbol, start_date.date(), dt.datetime.today().date())
 
-    df_stocks_prices['Date'] = df_stocks_prices['Date'].apply(lambda x: dt.datetime.strftime(x, '%Y-%m-%d'))
-    df_stocks_prices.drop(df_stocks_prices[df_stocks_prices.Date <= str_start_date].index, inplace=True)
+       df_stocks_prices.insert(0, 'Name', symbol + '.BA')
+       col_map = {df_stocks_prices.columns[1]: 'Date', 
+                  df_stocks_prices.columns[2]: 'Open', 
+                  df_stocks_prices.columns[3]: 'High', 
+                  df_stocks_prices.columns[4]: 'Low', 
+                  df_stocks_prices.columns[5]: 'Close', 
+                  df_stocks_prices.columns[6]: 'Volume'}
+       df_stocks_prices.rename(columns=col_map, inplace=True)
+       df_stocks_prices.insert(6, 'AdjClose', df_stocks_prices['Close'])
 
-    return df_stocks_prices.to_records(index=False)
+       df_stocks_prices['Date'] = df_stocks_prices['Date'].apply(lambda x: dt.datetime.strftime(x, '%Y-%m-%d'))
+       df_stocks_prices.drop(df_stocks_prices[df_stocks_prices.Date <= str_start_date].index, inplace=True)
+
+       result = df_stocks_prices.to_records(index=False)
+
+    return result
 
 def main():
 
-    database = r"stocks.db"
+    database = "stocks.db"
 
     hb = HomeBroker(265)
     hb_dni = input('DNI:')
@@ -110,17 +141,18 @@ def main():
 
     with conn:
         tickers = select_stocks_symbols(conn)
-        #tickers = [('GFGA.BA',)]
-        #tickers = [('^VIX',), ('^GSPC',)]
-        for ticker in tickers:
-            print(ticker[0])
+
+        for ticker in (pbar:=tqdm(tickers)):
+            pbar.set_postfix_str(ticker[0])
             try:
                 if ticker[0][-3:] == '.BA':
                     insert_stock_prices_from(conn, get_stock_prices_from_hb(conn, hb, ticker[0][:-3]))
                 else:
                     insert_stock_prices_from(conn, get_stock_prices_from_yf(conn, ticker[0]))
             except Error as e:
-                print(e)
+                pbar.write(str(e))
+
+    print("Proceso finalizado!21719832")
 
 if __name__ == '__main__':
     main()
